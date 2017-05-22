d1a5a88f5a192db69bf1ea481a28845d04526862
Szymon Kuczaty <skuczaty@gmail.com>
2017-05-22 19:32:40 +0200

    podstawowe działania

--- liczydlo/mainwindow.h
+++ liczydlo/mainwindow.h
@@ -15,8 +15,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_mc_clicked();
+
+    void on_c2_clicked();
+
+    void on_c7_clicked();
+
+    void digit_clicked();
+    void on_divide_clicked();
+
 private:
     Ui::MainWindow *ui;
+    bool waitForOperand;
+    double sum;
 };
 
 #endif // MAINWINDOW_H
